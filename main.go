c625cd2c9ac7741797536f8daf0cd6d1b5938d67
nProgrammer <wagner.norbert277@gmail.com>
2021-05-10 00:23:40 +0200

    added open and close vaults function

--- main.go
+++ main.go
@@ -1,28 +1,40 @@
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
-	"strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Name of vault: ")
-	name, _ := reader.ReadString('\n')
-	fmt.Println("Password for vault: ")
-	pass, _ := reader.ReadString('\n')
-	name = strings.TrimSpace(name)
-	pass = strings.TrimSpace(pass)
+	open := flag.Bool("o", false, "Open the vault")
+	close := flag.Bool("c", false, "Close the vault")
+	name := flag.String("n", "", "Name of the vault")
+	pass := flag.String("p", "", "Password for the vault")
+
+	flag.Parse()
+
 	path := os.Getenv("VAULT")
-	command1 := `unzip`
-	command2 := path + `/.saves/` + name + `.zip`
-	command3 := pass
-	cmd := exec.Command(command1, "-P", command3, command2)
-	fmt.Println(command1, command2, command3)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
+	fmt.Println(path)
+	fmt.Println(*open)
+	if *open {
+		command1 := `unzip`
+		command2 := path + `/.saves/` + *name + `.zip`
+		command3 := *pass
+		fmt.Println(command1, "-P", command3, command2)
+		cmd := exec.Command(command1, "-P", command3, command2)
+		fmt.Println(command1, command2, command3)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
+	} else if *close {
+		cmd := exec.Command("zip", "-re", "-P", *pass, *name+".zip", *name)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
+		cmd = exec.Command("mv", *name+".zip", path+"/.saves/")
+		cmd.Run()
+	}
+
 }
